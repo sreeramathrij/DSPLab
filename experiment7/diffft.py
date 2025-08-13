0a595116309a7a5fc74e5ad7762bd790abb712d6
Sreeram Athrij <sreeramathrij344@gmail.com>
2025-08-13 07:28:14 +0530

    Optimized experiment 7

--- experiment7/diffft.py
+++ experiment7/diffft.py
@@ -2,45 +2,50 @@ import numpy as np;
 import matplotlib.pyplot as plt;
 
 def bit_reverse_indices(N):
-  bits = int(np.log2(N))
-  reversal = np.zeros(N, dtype=int);
+    bits = int(np.log2(N))
+    reversal = np.zeros(N, dtype=int)
 
-  for i in range(N):
-    b = bin(i)[2:].zfill(bits)
-    reversal[i] = int(b[::-1], 2);
-
-  return reversal;
+    for i in range(N):
+      b = bin(i)[2:].zfill(bits)
+      reversal[i] = int(b[::-1], 2)
+    
+    return reversal
 
 def diffft(x):
   x = np.array(x, dtype=complex)
-  N = x.size
+  N = x.shape[0]
+  if N == 1:
+        return x
+  if N % 2 != 0:
+    print("size of x must be a power of 2")
+    return
+  even = x[:N//2] + x[N//2:]
+  odd  = (x[:N//2] - x[N//2:]) * np.exp(-2j * np.pi * np.arange(N//2) / N)
 
-  length = N
-  while length > 1:
-    m_by_2 = length // 2
-    W_m = np.exp(-2j * np.pi * np.arange(m_by_2) / length)
-    for i in range(0, N, length):
-      for j in range(m_by_2):
-        u = x[i + j]
-        v = x[i + j + m_by_2]
-        x[i + j] = u + v
-        x[i + j + m_by_2] = (u - v) * W_m[j]
 
-    length = m_by_2
+  X_even = diffft(even)
+  X_odd  = diffft(odd)
 
-  indices = bit_reverse_indices(N)
+  X = np.concatenate([X_even, X_odd])
 
-  return x[indices]
+  return X
 
 
 x = [1,2,3,4,5,6,7,8]
+X = diffft(x)
 
-fig, ax = plt.subplots(2, 1);
+N = X.size
 
-ax[0].stem(np.abs(diffft(x)));
-ax[1].stem(np.abs(np.fft.fft(x)));
+bit_rev = bit_reverse_indices(N)
+
+X_ordered = np.zeros(N, dtype=complex)
+for i in range(N):
+  X_ordered[bit_rev[i]] = X[i]
+
+fig, ax = plt.subplots(2,1)
+
+ax[0].stem(np.abs(X_ordered))
+ax[1].stem(np.abs(np.fft.fft(x)))
 
 fig.tight_layout()
-plt.show();
- 
-        
+plt.show()
--- experiment7/ditfft.py
+++ experiment7/ditfft.py
@@ -1,43 +1,32 @@
 import numpy as np;
 import matplotlib.pyplot as plt;
 
-def bit_reverse_indices(N):
-  bits = int(np.log2(N))
-  reversal = np.zeros(N, dtype=int);
-
-  for i in range(N):
-    b = bin(i)[2:].zfill(bits)
-    reversal[i] = int(b[::-1], 2);
-
-  return reversal;
-
-def ditfft(x):
+def fft(x):
   x = np.array(x, dtype=complex)
   N = x.size
 
-  indices = bit_reverse_indices(N)
-  X = x[indices].copy()
+  if N == 1:
+    return x
+  if np.log2(N) % 1 != 0:
+    print("size of x must be a power of 2")
+    return
+  
+  X_even = fft(x[::2])
+  X_odd = fft(x[1::2])
 
-  stages = int(np.log2(N))
-  for s in range(1, stages+1):
-    m = 2**s
-    m_by_2 = m // 2
-    W_m = np.exp(-2j * np.pi * np.arange(m_by_2) / m)
-    for k in range(0, N, m):
-      for j in range(m_by_2):
-        t = W_m[j] * X[k + j + m_by_2]
-        u = X[k+j]
-        X[k+j] = u + t
-        X[k + j + m_by_2] = u - t 
+  factor = np.exp(-2j * np.pi * np.arange(N) / N)
 
-  return X
+  return np.concatenate([X_even + factor[:N // 2] * X_odd, X_even - factor[:N//2] * X_odd])
+
 
 x = [1,2,3,4,5,6,7,8]
 
 fig, ax = plt.subplots(2, 1);
 
-ax[0].stem(np.abs(ditfft(x)));
+
+ax[0].stem(np.abs(fft(x)));
 ax[1].stem(np.abs(np.fft.fft(x)));
 
 fig.tight_layout()
 plt.show();
+ 
